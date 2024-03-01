85ab9ea2a54db68feefb23b31c8bccb646f66d21
limingkai <lingqi.lmk@alibaba-inc.com>
2024-03-01 11:15:17 +0800

    [update]clean up go modules

--- infer-module/controller/init.go
+++ infer-module/controller/init.go
@@ -1,4 +1,14 @@
 package controller
 
+import "sync"
+
 type ParallelInferer struct {
+	mu       sync.Mutex
+	services []Service
+}
+
+type Service struct {
+	ServiceName string
+	ModelName   string
+	Tags        string
 }
