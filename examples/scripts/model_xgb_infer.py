ceab83d656f69fc18e5f96777b654a2b0fba8b98
limingkai <lingqi.lmk@alibaba-inc.com>
2024-03-12 13:34:22 +0800

    [update] complete xgboost model

--- examples/scripts/model_xgb_infer.py
+++ examples/scripts/model_xgb_infer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import argparse
 from datetime import datetime
+import pickle
+import xgboost as xgb
+import numpy as np
 
 
 def parse_args_infer() -> argparse.Namespace:
@@ -13,3 +16,31 @@ def parse_args_infer() -> argparse.Namespace:
     parser.add_argument("-m", "--model", type=str, required=True)
 
     return parser.parse_args()
+
+
+def prepare_data(timestamp: str, value: float):
+    t = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+    t_ref = t.replace(hour=0, minute=0, second=0)
+    diff_sec = (t - t_ref).total_seconds()
+    vec = list()
+    for i in range(3):
+        v = diff_sec - (3-i)*15
+        if v < 0:
+            vec.append(0)
+        else:
+            vec.append(v)
+    vec.append(diff_sec)
+    vec.append(value)
+    return xgb.DMatrix(np.array([vec]))
+
+
+def load_model(model_path: str) -> xgb.Booster:
+    with open(model_path, "rb") as f:
+        return pickle.load(f)
+
+if __name__ == "__main__":
+    args = parse_args_infer()
+    data = prepare_data(args.timestamp, args.value)
+    model = load_model(args.model)
+    forecast = model.predict(data)
+    print(forecast[0])
