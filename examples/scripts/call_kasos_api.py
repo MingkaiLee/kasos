ed5f5c5751bf50d08988aca3f150dcf4a29e46e6
limingkai <lingqi.lmk@alibaba-inc.com>
2024-03-13 16:24:28 +0800

    [update] complete caller python scripts of kasos in examples

--- examples/scripts/call_kasos_api.py
+++ examples/scripts/call_kasos_api.py
@@ -2,5 +2,109 @@ import requests
 
 HOST_PORT = "http://localhost:10168"
 
-def service_find(name):
-    requests.get(f"{HOST_PORT}/service-manager/find?name={name}")
+
+def service_find(name: str):
+    response = requests.get(f"{HOST_PORT}/service-manager/find?name={name}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /service-manager/find failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def list_services(index: int):
+    response = requests.get(f"{HOST_PORT}/service-manager/list?index={index}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /service-manager/list failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def register_service(name: str, tags: map[str, str], model_name: str):
+    response = requests.post(f"{HOST_PORT}/service-manager/register",
+                             json={
+                                 "name": name,
+                                 "tags": tags,
+                                 "model_name": model_name
+                             })
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /service-manager/register failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def register_service_result(name: str):
+    response = requests.get(
+        f"{HOST_PORT}/service-manager/register-result?name={name}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /service-manager/register-result failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def delete_service(name: str):
+    response = requests.post(f"{HOST_PORT}/service-manager/delete?name={name}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /service-manager/delete failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def find_model(name: str):
+    response = requests.get(f"{HOST_PORT}/model-manager/find?name={name}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /model-manager/find failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def list_models(index: int):
+    response = requests.get(f"{HOST_PORT}/model-manager/list?index={index}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /model-manager/list failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def register_model(name: str, train_script: str, infer_script: str):
+    response = requests.post(f"{HOST_PORT}/model-manager/register",
+                             json={
+                                 "name": name,
+                                 "trainScript": train_script,
+                                 "inferScript": infer_script
+                             })
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /model-manager/register failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def register_model_result(name: str):
+    response = requests.get(
+        f"{HOST_PORT}/model-manager/register-result?name={name}")
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /model-manager/register-result failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
+
+
+def fetch_data(start_time: str, end_time: str, tags: str):
+    response = requests.post(f"{HOST_PORT}/data-manager/fetch",
+                             json={
+                                 "start_time": start_time,
+                                 "end_time": end_time,
+                                 "tags": tags
+                             })
+    if response.status_code != 200:
+        raise Exception(
+            f"call kasos api /data-manager/fetch failed, code: {response.status_code}, msg: {response.text}"
+        )
+    return response.json()
