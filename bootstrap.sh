ab3b12eda371c4a0ceb635b29d621dea464953f4
limingkai <852834777@qq.com>
2024-03-14 21:23:11 +0800

    [update] modify resource allocations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 git submodule update --init
 cd ./kube-prometheus
-minikube start --kubernetes-version=v1.28.0 --memory=16g --cpus=10 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.bind-address=0.0.0.0 --extra-config=controller-manager.bind-address=0.0.0.0
+minikube start --kubernetes-version=v1.28.0 --memory=12g --cpus=8 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.bind-address=0.0.0.0 --extra-config=controller-manager.bind-address=0.0.0.0
 cd ..
 cd ./build
 ./build.sh
