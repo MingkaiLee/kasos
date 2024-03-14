c429677c88fb06129b6f694cd3238ff7d2fbefae
limingkai <lingqi.lmk@alibaba-inc.com>
2024-03-14 16:09:30 +0800

    [update] make build script for example service

---
+++ build/build_example_service/build-example-service.sh
@@ -0,0 +1,25 @@
+# Author: lmk17@tsinghua.org.cn
+# Date: 2024-03-10
+# Description: Deploy service whose name is measure in example directory
+#!/usr/bin/bash
+
+build_image() {
+    local image_path=$1
+    local image_name=$2
+    local image_tag=$3
+    local work_dir=$(pwd)
+    cd $image_path
+    docker build -t $image_name:$image_tag .
+    cd $work_dir
+}
+
+cmd_log() {
+    local cmd=$1
+    echo ">>> $cmd"
+    eval $cmd
+}
+
+build_image "../../example/measure" "example-measure" "v0.1"
+
+cmd_log "kubectl apply -f ./measure-deployment.yaml"
+cmd_log "kubectl apply -f ./measure-service.yaml"
