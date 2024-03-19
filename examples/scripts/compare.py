76d792239aab5dbc5d58ba04bac3ca3e9ce0b693
limingkai <lingqi.lmk@alibaba-inc.com>
2024-03-19 14:48:22 +0800

    [update] update some scripts and results for time series evaluation

--- examples/scripts/compare.py
+++ examples/scripts/compare.py
@@ -1,1 +1,49 @@
 #!/usr/bin/python3
+import argparse
+import numpy as np
+from dtaidistance import dtw
+from dtaidistance import dtw_visualisation as dtwvis
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="draw time series")
+    parser.add_argument("-b",
+                        "--observe",
+                        type=str,
+                        required=True,
+                        help="observe data path")
+    parser.add_argument("-d",
+                        "--data",
+                        type=str,
+                        required=True,
+                        help="infer data path")
+    parser.add_argument("-o",
+                        "--output",
+                        type=str,
+                        required=True,
+                        help="output image file path")
+
+    return parser.parse_args()
+
+
+def prepare_data(data_path: str) -> list[float]:
+    r = list()
+    with open(data_path, "r") as f:
+        r = [float(line.strip().split("\t")[1]) for line in f.readlines()]
+
+    return r
+
+
+start = 4 * 60 * 12
+end = 4 * 60 * 12 + 4 * 30
+
+if __name__ == "__main__":
+    args = parse_args()
+    ob_data = np.array(prepare_data(args.observe)[1:])
+    infer_data = np.array(prepare_data(args.data)[:-1])
+    distance = dtw.distance(ob_data, infer_data, use_pruning=True)
+    print(f"dtw distance: {distance}")
+    ob_data_vis = ob_data[start:end]
+    infer_data_vis = infer_data[start:end]
+    path = dtw.warping_path(ob_data_vis, infer_data_vis)
+    dtwvis.plot_warping(ob_data_vis, infer_data_vis, path, args.output)
--- examples/scripts/draw_time_series.py
+++ examples/scripts/draw_time_series.py
@@ -7,21 +7,29 @@ import os.path as osp
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="draw time series")
+    parser = argparse.ArgumentParser(description="draw time series")
     parser.add_argument("-d",
                         "--data",
                         nargs="*",
                         type=str,
                         required=True,
                         help="data path")
+    parser.add_argument("-o",
+                        "--output",
+                        type=str,
+                        required=True,
+                        help="output image file path")
 
     return parser.parse_args()
 
 
 def read_data(data_path: str) -> pd.DataFrame:
     name = osp.split(data_path)[1].split(".")[0]
-    return pd.read_csv(data_path, sep="\t", index_col=0, header=None, names=[name])
+    return pd.read_csv(data_path,
+                       sep="\t",
+                       index_col=0,
+                       header=None,
+                       names=[name])
 
 
 if __name__ == "__main__":
@@ -32,13 +40,13 @@ if __name__ == "__main__":
         dfs.append(df)
     data = dfs[0].join(dfs[1:], how="outer")
     data.index = [x.split(" ")[1] for x in data.index]
-    plt_data = data.head(100)
+    plt_data = data.iloc[4 * 60 * 12 - 1:4 * 60 * 12 + 4 * 30]
     sns.set_style("darkgrid")
     sns.lineplot(data=plt_data)
     plt.xticks(rotation=45, fontsize=8)
     for index, label in enumerate(plt.gca().xaxis.get_ticklabels()):
-        if index % 5 != 0:
+        if index % 8 != 0:
             label.set_visible(False)
     plt.tight_layout()
     plt.legend()
-    plt.show()
+    plt.savefig(args.output)
